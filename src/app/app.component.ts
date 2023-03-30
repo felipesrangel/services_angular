8f747d606806e37013224f207939d2ed88f9dbbb
Felipe Rangel <78965577+felipesrangel@users.noreply.github.com>
2023-03-30 00:07:55 -0300

    criando service na aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,11 +24,13 @@ import { Component, OnInit} from '@angular/core';
   <br>
   <button (click)="add()"> Add </button>
 -->
-  <ng-template [ngIf]="getDados"]>
+  <!-- <ng-template [ngIf]="getDados"]>
     <h1>{{getDados.nome}}</h1>
     <h2> {{getDados.idade}}</h2>
   </ng-template>
-  <app-output (enviarDados)="setDados($event)"></app-output>
+  <app-output (enviarDados)="setDados($event)"></app-output> -->
+
+  <app-food-list></app-food-list>
   <router-outlet></router-outlet>
   `
 })
---
+++ src/app/services/food-list.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodListService {
+
+  private list:Array<string> = [
+    "X Bacon",
+    "Feijão",
+    "Ovo"
+  ];
+  constructor() { }
+
+  public foodList(){
+    return this.list;
+  }
+}
