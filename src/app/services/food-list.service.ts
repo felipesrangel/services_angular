c30d8c504d5de35c7fd70caa5e20a99e1a5c84da
Felipe Rangel <78965577+felipesrangel@users.noreply.github.com>
2023-04-01 15:14:35 -0300

    criando http-get

--- src/app/services/food-list.service.ts
+++ src/app/services/food-list.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FoodList } from '../module/food-list';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +15,19 @@ export class FoodListService {
     "Feijão",
     "Ovo"
   ];
-  constructor() { }
 
-  public foodList(){
-    return this.list;
+  private url: string = "http://localhost:3000/"; //list-food
+  constructor( private http: HttpClient) { }
+
+  // public foodList(){
+  //   return this.list;
+  // }
+
+  public foodList():Observable<FoodList>{
+    return this.http.get<FoodList>(`${this.url}list-food`).pipe(
+      res => res,
+      error => error
+    )
   }
 
   public foodListAdd(value: string){
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HttpClientModule } from '@angular/common/http';
 //Components
 import { NewComponentComponent } from './new-component/new-component.component';
 import { InputComponent } from './input/input.component';
@@ -26,7 +26,8 @@ import { FoodAddComponent } from './food-add/food-add.component';
     FoodAddComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ]
 })
 export class SharedModule { }
