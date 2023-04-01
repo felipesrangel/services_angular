4d94e99d583cef8356be51ceeff84cbbf85db3dd
Felipe Rangel <78965577+felipesrangel@users.noreply.github.com>
2023-04-01 16:24:55 -0300

    criando delete e put

--- src/app/services/food-list.service.ts
+++ src/app/services/food-list.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
+//Module Interface
 import { FoodList } from '../module/food-list';
 
 @Injectable({
@@ -15,6 +17,12 @@ export class FoodListService {
     "Feijão",
     "Ovo"
   ];
+
+  private httpOptions = {
+    header: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  }
 
   private url: string = "http://localhost:3000/"; //list-food
   constructor( private http: HttpClient) { }
@@ -37,6 +45,19 @@ export class FoodListService {
 
   public foodListAdd(value: string): Observable<FoodList>{
     return this.http.post<FoodList>(`${this.url}list-food`, {nome: value}).pipe(
+      res => res,
+      error => error
+    )
+  }
+
+  public foodListEdit(value:string, id: number): Observable<FoodList>{
+    return this.http.put<FoodList>(`${this.url}list-food/${id}`, {nome: value}).pipe(
+      res => res,
+      error => error
+    )
+  }
+  public foodListDelete(id: number): Observable<FoodList>{
+    return this.http.delete<FoodList>(`${this.url}list-food/${id}`).pipe(
       res => res,
       error => error
     )
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { InputComponent } from './input/input.component';
 import { OutputComponent } from './output/output.component';
 import { FoodListComponent } from './food-list/food-list.component';
 import { FoodAddComponent } from './food-add/food-add.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -27,7 +28,8 @@ import { FoodAddComponent } from './food-add/food-add.component';
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ]
 })
 export class SharedModule { }
